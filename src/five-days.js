cf428bebc2d93c1509f8ab20fbdcb354ba0fd035
Otroletravaladna <crnana98@gmail.com>
2023-03-09 01:15:52 -0300

    Delete min temp in five days forecast and add rain probability

--- src/five-days.js
+++ src/five-days.js
@@ -4,7 +4,7 @@ export async function getDaysWeather(data) {
     const result = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=12ba82103cee621b422b9bd770104e91&units=metric`);
     const forecast = await result.json();
     displayDays(forecast);
-    console.log(forecast);
+    console.log(forecast)
 }
 
 function displayDays(forecast){
@@ -19,64 +19,64 @@ function displayOne(forecast) {
     const date = document.querySelector(".date-one");
     const main = document.querySelector(".main-one");
     const max = document.querySelector(".max-one");
-    const min = document.querySelector(".min-one");
+    const pop = document.querySelector(".pop-one");
 
     const day = addDays(new Date(forecast.list[3].dt_txt), 1);
     date.textContent = format(day, "eeee dd");
     main.textContent = forecast.list[3].weather[0].description;
     max.textContent = `Max: ${forecast.list[3].main.temp_max}°`;
-    min.textContent = `Min: ${forecast.list[3].main.temp_min}°`;
+    pop.textContent = `Rain probability: ${forecast.list[3].pop * 100}%`;
 }
 
 function displayTwo(forecast) {
     const date = document.querySelector(".date-two");
     const main = document.querySelector(".main-two");
     const max = document.querySelector(".max-two");
-    const min = document.querySelector(".min-two");
+    const pop = document.querySelector(".pop-two");
 
     const day = addDays(new Date(forecast.list[11].dt_txt), 1);
     date.textContent = format(day, "eeee dd");
     main.textContent = forecast.list[11].weather[0].description;
     max.textContent = `Max: ${forecast.list[11].main.temp_max}°`;
-    min.textContent = `Min: ${forecast.list[11].main.temp_min}°`;
+    pop.textContent = `Rain probability: ${forecast.list[11].pop * 100}%`;
 }
 
 function displayThree(forecast) {
     const date = document.querySelector(".date-three");
     const main = document.querySelector(".main-three");
     const max = document.querySelector(".max-three");
-    const min = document.querySelector(".min-three");
+    const pop = document.querySelector(".pop-three");
 
     const day = addDays(new Date(forecast.list[19].dt_txt), 1);
     date.textContent = format(day, "eeee dd");
     main.textContent = forecast.list[19].weather[0].description;
     max.textContent = `Max: ${forecast.list[19].main.temp_max}°`;
-    min.textContent = `Min: ${forecast.list[19].main.temp_min}°`;
+    pop.textContent = `Rain probability: ${forecast.list[19].pop * 100}%`;
 }
 
 function displayFour(forecast) {
     const date = document.querySelector(".date-four");
     const main = document.querySelector(".main-four");
     const max = document.querySelector(".max-four");
-    const min = document.querySelector(".min-four");
+    const pop = document.querySelector(".pop-four");
 
     const day = addDays(new Date(forecast.list[27].dt_txt), 1);
     date.textContent = format(day, "eeee dd");
     main.textContent = forecast.list[27].weather[0].description;
     max.textContent = `Max: ${forecast.list[27].main.temp_max}°`;
-    min.textContent = `Min: ${forecast.list[27].main.temp_min}°`;
+    pop.textContent = `Rain probability: ${forecast.list[27].pop * 100}%`;
 }
 
 function displayFive(forecast){
     const date = document.querySelector(".date-five");
     const main = document.querySelector(".main-five");
     const max = document.querySelector(".max-five");
-    const min = document.querySelector(".min-five");
+    const pop = document.querySelector(".pop-five");
 
     const day = addDays(new Date(forecast.list[35].dt_txt), 1);
     date.textContent = format(day, "eeee dd");
     main.textContent = forecast.list[35].weather[0].description;
     max.textContent = `Max: ${forecast.list[35].main.temp_max}°`;
-    min.textContent = `Min: ${forecast.list[35].main.temp_min}°`;
+    pop.textContent = `Rain probability: ${forecast.list[35].pop * 100}%`;
 }
 
