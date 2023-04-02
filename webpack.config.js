68971febd7cef2eb44244257a6c6735d9859f991
Otroletravaladna <crnana98@gmail.com>
2023-04-02 19:42:25 -0300

    Edit webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,12 @@ module.exports = {
           },
           {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images',
+              publicPath: 'images',
+            },
           },
         ],
       },
